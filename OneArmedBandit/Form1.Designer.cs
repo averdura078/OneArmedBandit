1967b67bf26e7334d59a1505cf90c056e6b155dc
Tony Theodoropoulos <theodot@outlook.com>
2024-11-18 11:22:40 -0500

    Add project files.

---
+++ OneArmedBandit/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace OneArmedBandit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.spinButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.scoreDisplay = new System.Windows.Forms.Label();
+            this.coinLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // spinButton
+            // 
+            this.spinButton.BackColor = System.Drawing.Color.LimeGreen;
+            this.spinButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.spinButton.Font = new System.Drawing.Font("Rockwell", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.spinButton.Location = new System.Drawing.Point(319, 122);
+            this.spinButton.Margin = new System.Windows.Forms.Padding(2);
+            this.spinButton.Name = "spinButton";
+            this.spinButton.Size = new System.Drawing.Size(51, 46);
+            this.spinButton.TabIndex = 3;
+            this.spinButton.Text = "SPIN";
+            this.spinButton.UseVisualStyleBackColor = false;
+            this.spinButton.Click += new System.EventHandler(this.spinButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.DimGray;
+            this.outputLabel.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(104, 236);
+            this.outputLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(162, 31);
+            this.outputLabel.TabIndex = 4;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // scoreDisplay
+            // 
+            this.scoreDisplay.BackColor = System.Drawing.Color.Transparent;
+            this.scoreDisplay.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreDisplay.ForeColor = System.Drawing.Color.White;
+            this.scoreDisplay.Location = new System.Drawing.Point(59, 236);
+            this.scoreDisplay.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.scoreDisplay.Name = "scoreDisplay";
+            this.scoreDisplay.Size = new System.Drawing.Size(41, 31);
+            this.scoreDisplay.TabIndex = 5;
+            this.scoreDisplay.Text = "10";
+            this.scoreDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // coinLabel
+            // 
+            this.coinLabel.BackColor = System.Drawing.Color.Transparent;
+            this.coinLabel.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.coinLabel.ForeColor = System.Drawing.Color.White;
+            this.coinLabel.Location = new System.Drawing.Point(11, 235);
+            this.coinLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.coinLabel.Name = "coinLabel";
+            this.coinLabel.Size = new System.Drawing.Size(56, 31);
+            this.coinLabel.TabIndex = 6;
+            this.coinLabel.Text = "Coins:";
+            this.coinLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::OneArmedBandit.Properties.Resources._7_100x125;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(84, 115);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(54, 56);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::OneArmedBandit.Properties.Resources.cherry_100x125;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(159, 115);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(54, 56);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImage = global::OneArmedBandit.Properties.Resources.diamond_100x125;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(235, 115);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(54, 56);
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::OneArmedBandit.Properties.Resources.slotBack;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(373, 268);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.coinLabel);
+            this.Controls.Add(this.scoreDisplay);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.spinButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "One Armed Bandit";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button spinButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label scoreDisplay;
+        private System.Windows.Forms.Label coinLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ OneArmedBandit/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OneArmedBandit
+{
+    public partial class Form1 : Form
+    {
+        // random number generator and int value for score
+        Random randGenerator = new Random();
+        int score = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void spinButton_Click(object sender, EventArgs e)
+        {
+            // get random values for each reel
+            int reel1Value = randGenerator.Next(1, 4);
+            int reel2Value = randGenerator.Next(1, 4);
+            int reel3Value = randGenerator.Next(1, 4);
+
+            // check value of reel 1 with a switch statement and set appropriate image
+            switch (reel1Value)
+            {
+                case 1:
+                    pictureBox1.BackgroundImage = Properties.Resources.cherry_100x125;
+                    break;
+                case 2:
+                    pictureBox1.BackgroundImage = Properties.Resources.diamond_100x125;
+                    break;
+                case 3:
+                    pictureBox1.BackgroundImage = Properties.Resources._7_100x125;
+                    break;
+            }
+
+            // check value of reel 2 with a switch statement and set appropriate image
+            switch (reel2Value)
+            {
+                case 1:
+                    pictureBox2.BackgroundImage = Properties.Resources.cherry_100x125;
+                    break;
+                case 2:
+                    pictureBox2.BackgroundImage = Properties.Resources.diamond_100x125;
+                    break;
+                case 3:
+                    pictureBox2.BackgroundImage = Properties.Resources._7_100x125;
+                    break;
+            }
+
+            // check value of reel 3 with a switch statement and set appropriate image
+            switch (reel3Value)
+            {
+                case 1:
+                    pictureBox3.BackgroundImage = Properties.Resources.cherry_100x125;
+                    break;
+                case 2:
+                    pictureBox3.BackgroundImage = Properties.Resources.diamond_100x125;
+                    break;
+                case 3:
+                    pictureBox3.BackgroundImage = Properties.Resources._7_100x125;
+                    break;
+            }
+
+            // use compound if statement to check if all reels are equal 
+            // in all same show "winner" statement and add 3 to score
+            // else show "play again" statement and subtract 1 from score         
+            if (reel1Value == reel2Value && reel1Value == reel3Value)
+            {
+                outputLabel.Text = "WINNER!!";
+                score = score + 3;
+            }
+            else
+            {
+                outputLabel.Text = "Play Again?";
+                score--;
+            }
+
+            // if score has reached 0 display "lose" message and set button enabled property to false
+            if (score <= 0)
+            {
+                outputLabel.Text = "You Lose";
+                spinButton.Enabled = false;
+            }
+
+            // display updated score
+            scoreDisplay.Text = Convert.ToString(score);
+        }
+    }
+}
